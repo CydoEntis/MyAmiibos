8d5fccaf2b1b7b43638b268f62e2cc9dffb1dd31
Cydo <cydoentis@gmail.com>
2022-09-01 18:59:00 -0400

    Added custom error classes

---
+++ server/errors/bad-request.js
@@ -0,0 +1,9 @@
+import CustomError from "./custom-error";
+import { StatusCodes } from "http-status-codes";
+
+class BadRequestError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
