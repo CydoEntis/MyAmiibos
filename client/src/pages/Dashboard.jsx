a324b3215e2a10615b924eef7f972476a3289210
Cydo <cydoentis@gmail.com>
2022-09-17 22:46:40 -0400

    Fixed UI for list of amiibos

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,69 +1,9 @@
-import axios from 'axios';
 import React from 'react';
-import { useState, useEffect, useCallback } from 'react';
-import AmiiboCard from '../components/Amiibo/AmiiboCard';
-import classes from './Dashboard.module.css';
-import Card from '../components/UI/Cards/Card';
-import Pagination from '../components/Pagination/Pagination';
+import AmiiboList from '../components/Amiibo/AmiiboList';
+
 
 const Dashboard = () => {
-	const limit = 25;
-	const [currentPage, setCurrentPage] = useState(1);
-	const [allAmiibos, setAllAmiibos] = useState([]);
-	// const [pages, setPages] = useState([]);
-	const indexOfLastAmiibo = currentPage * limit;
-	const indexOfFirstAmiibo = indexOfLastAmiibo - limit;
-	const currentAmiibos = allAmiibos.slice(
-		indexOfFirstAmiibo,
-		indexOfLastAmiibo
-	);
-	const numOfPages = Math.ceil(allAmiibos.length / limit);
 
-	useEffect(() => {
-		let cancelled = false;
-		const getAmiibos = async () => {
-			const { data } = await axios.get(
-				'https://www.amiiboapi.com/api/amiibo'
-			);
-			if (!cancelled) {
-				setAllAmiibos(data.amiibo);
-			}
-		};
-
-		getAmiibos();
-
-		return () => {
-			cancelled = true;
-		};
-	}, []);
-
-	// const nextPage = () => {
-	// 	setPage((prevState) => prevState + 1);
-	// };
-
-	// const skip = (page - 1) * limit;
-	// const currentAmiibos = allAmiibos?.slice?.(skip, skip + limit) ?? [];
-
-	return (
-		<div className={classes.list}>
-			<Pagination
-				numOfPages={numOfPages}
-				maxPages={4}
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
-			/>
-			{allAmiibos !== null && (
-				<div className={classes.list}>
-					{currentAmiibos.map((amiibo, index) => (
-						<AmiiboCard
-							amiibo={amiibo}
-							index={index}
-							key={amiibo.head + amiibo.tail}
-						/>
-					))}
-				</div>
-			)}
-		</div>
-	);
+	return <AmiiboList />;
 };
 export default Dashboard;
