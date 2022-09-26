f0b393318367ea39167dde32695bb5500456a508
Cydo <cydoentis@gmail.com>
2022-09-25 20:47:06 -0400

    Fixed logo animation, Finished new Logo

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -6,9 +6,7 @@ import classes from "./Logo.module.css";
 const Logo = ({className}) => {
   const logoClasses = `${className} ${classes["logo-container"]}`;
   return (
-    <div className={logoClasses}>
       <h3 className={classes.logo}>Amiibo<span>Party</span></h3>
-    </div>
   )
 }
 
