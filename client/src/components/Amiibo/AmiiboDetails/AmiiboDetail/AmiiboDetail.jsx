776c92e90954bfb59aba110b4066c2f37e6d0440
Cydo <cydoentis@gmail.com>
2022-09-26 20:13:31 -0400

    Restructured amiibo's folder structure

---
+++ client/src/components/Amiibo/AmiiboDetails/AmiiboDetail/AmiiboDetail.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ReactDom from 'react-dom';
+import { useAppContext } from '../../../../context/appContext';
+import Button from '../../../UI/Buttons/Button';
+
+import classes from './AmiiboDetail.module.css';
+import { FaTimes } from 'react-icons/fa';
+import DetailText from '../DetailText/DetailText';
+
+const Overlay = () => {
+	return <div className={classes.overlay}></div>;
+};
+
+const AmiiboDetail = () => {
+	const { selectedAmiibo, showDetails, hideAmiiboDetails } = useAppContext();
+
+	const onClose = () => {
+		hideAmiiboDetails();
+	};
+
+	if (!showDetails) return;
+
+	console.log(selectedAmiibo);
+
+	return ReactDom.createPortal(
+		<>
+			<Overlay />
+			<div className={classes.details}>
+				<Button className={classes['btn--close']} onClick={onClose}>
+					<FaTimes className={classes['icon--close']} />
+				</Button>
+				<div className={classes['detail__image-wrapper']}>
+					<img
+						src={selectedAmiibo.image}
+						alt={selectedAmiibo.character}
+					/>
+				</div>
+
+				<h3>{selectedAmiibo.name}</h3>
+				<DetailText category='Type' text={selectedAmiibo.type} />
+				<DetailText category='Game Series' text={selectedAmiibo.gameSeries} />
+				<DetailText category='Release Date' text={selectedAmiibo.release.na} />
+				
+			</div>
+			{/* <Modal
+				open={isOpen}
+				onClick={() => {
+					setIsOpen(true);
+				}}
+				onClose={() => {
+					setIsOpen(false);
+				}}
+			>
+				Text
+			</Modal> */}
+		</>,
+		document.getElementById('modal')
+	);
+};
+
+export default AmiiboDetail;
