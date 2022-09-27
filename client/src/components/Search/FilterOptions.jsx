ba38ca1c6e1ba33131dfd5ffe69efb660444a76a
Cydo <cydoentis@gmail.com>
2022-09-27 13:07:58 -0400

    Added media queries for cards, and pagination

--- client/src/components/Search/FilterOptions.jsx
+++ client/src/components/Search/FilterOptions.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import Button from '../UI/Buttons/Button';
 import Dropdown from '../UI/Dropdown/Dropdown';
 
-import classes from './FilterOptions.module.css';
 
 const FilterOptions = () => {
 
 	return (
 		<Dropdown></Dropdown>
-		// <div>
-		// 	<div className={classes['filter-options']}>
-		// 		<Button
-		// 			className={`${classes.btn} ${classes['btn--all']}`}
-		// 			onClick={getAllAmiibos}
-		// 		>
-		// 			All
-		// 		</Button>
-		// 		<Button
-		// 			className={`${classes.btn} ${classes['btn--figures']}`}
-		// 			onClick={getAllFigures}
-		// 		>
-		// 			Figures
-		// 		</Button>
-		// 		<Button
-		// 			className={`${classes.btn} ${classes['btn--cards']}`}
-		// 			onClick={getAllCards}
-		// 		>
-		// 			Cards
-		// 		</Button>
-		// 		<Button
-		// 			className={`${classes.btn} ${classes['btn--yarn']}`}
-		// 			onClick={getAllYarn}
-		// 		>
-		// 			Yarn
-		// 		</Button>
-		// 	</div>
-		// </div>
 	);
 };
 
