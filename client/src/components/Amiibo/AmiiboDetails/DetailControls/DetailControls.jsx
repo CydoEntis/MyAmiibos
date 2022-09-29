533cc53d2cca0c1726e5a4102e6fae9715048120
Cydo <cydoentis@gmail.com>
2022-09-29 08:58:15 -0400

    Added new dashboard

--- client/src/components/Amiibo/AmiiboDetails/DetailControls/DetailControls.jsx
+++ client/src/components/Amiibo/AmiiboDetails/DetailControls/DetailControls.jsx
@@ -3,18 +3,21 @@ import Button from '../../../UI/Buttons/Button';
 import { Link } from 'react-router-dom';
 
 import classes from './DetailControls.module.css';
+import { useAppContext } from '../../../../context/appContext';
 
 const DetailControls = () => {
+	const { hideAmiiboDetails } = useAppContext();
+
 	return (
 		<div className={classes.controls}>
 			<Button className={classes['btn--add']}>
 				Add To My Collection
 			</Button>
-			<Link className={classes.link} to='/collection'>
+			<Link className={classes.link} to='/collection' onClick={hideAmiiboDetails}>
 				View My Collection
 			</Link>
 			<Button className={classes['btn--add']}>Add To My Wishlist</Button>
-			<Link className={classes.link} to='/wishlist'>
+			<Link className={classes.link} to='/wishlist' onClick={hideAmiiboDetails}>
 				View My Wishlist
 			</Link>
 		</div>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navigation/Navbar';
 import MainWrapper from '../components/Wrappers/MainWrapper';
 import Footer from '../components/Footer/Footer';
 
+import AmiiboList from '../assets/images/amiibo-list.png';
+import Mario from '../assets/images/mario-amiibo.jpg';
+
 import classes from './Dashboard.module.css';
-import HeroWrapper from '../components/Wrappers/HeroWrapper';
+import { useAppContext } from '../context/appContext';
 
 const Dashboard = () => {
+	const { user } = useAppContext();
 	return (
 		<>
 			<Navbar />
-			<MainWrapper className={classes.dashboard}>
-				<HeroWrapper />
-			</MainWrapper>
+			<div className={classes.dashboard}>
+				<div className={classes['hero-section']}>
+					<h3>AmiiboParty is an Amiibo Collection App.</h3>
+					<h5>Keep track of your collected and wishlisted Amiibos</h5>
+					<Link to={user ? '/amiibos' : '/auth'} className={classes.btn}>
+						Start Collecting
+					</Link>
+					<div className={classes['img-wrapper']}>
+						<img className={classes['mobile-img']} src={Mario} alt='mario' />
+						<img className={classes['desktop-img']} src={AmiiboList} alt='list of amiibos' />
+					</div>
+				</div>
+			</div>
 			<Footer />
 		</>
 	);
