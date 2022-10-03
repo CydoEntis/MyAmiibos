c2f37f38979709433328eeff74ed3095bd3e2fd1
Cydo <cydoentis@gmail.com>
2022-10-03 19:51:44 -0400

    Fixed typo on error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -10,7 +10,7 @@ const Error = () => {
 			<img src={NotFound} alt='mario question mark block' />
 			<div className={classes['not-found--message']}>
 				<h3>Uh Oh! Page Not Found!</h3>
-				<p>We can't see to find the page you're looking for.</p>
+				<p>We can't seem to find the page you're looking for.</p>
 				<Link className={classes['btn--back']} to='/'>
 					back home
 				</Link>
