31b6e04a08446ea77a2ef43cbc37978a6077b522
Cydo <cydoentis@gmail.com>
2022-10-03 20:04:40 -0400

    Added Sorting and Page selection to options

--- client/src/components/Forms/AmiiboForm.jsx
+++ client/src/components/Forms/AmiiboForm.jsx
@@ -7,23 +7,34 @@ import classes from './AmiiboForm.module.css';
 
 const AmiiboForm = () => {
 	return (
-
-			<div className={classes['form--container']}>
-				<SearchBar />
-				<div className={classes.form}>
-					<div className={classes['form--filter']}>
-						<h3>Filter: </h3> <Filter />
+		<div className={classes['form--container']}>
+			<SearchBar />
+			<div className={classes.form}>
+				<div className={classes['form--filter']}>
+					<div className={classes['form--row']}>
+						<h3>Filter: </h3>
+						<Filter />
 					</div>
-					<div className={classes['form--buttons']}>
+					<div className={classes['form--row']}>
+						<h3>Sort: </h3>
+						<Button className={classes['form--button']}>A-Z</Button>
 						<Button className={classes['form--button']}>
-							My Wishlist
+							Series
 						</Button>
 						<Button className={classes['form--button']}>
-							My Collection
+							Date
 						</Button>
 					</div>
 				</div>
+
+				<div className={classes['form--buttons']}>
+					<h3>Cards Per Page: </h3>
+					<Button className={classes['form--button']}>25</Button>
+					<Button className={classes['form--button']}>50</Button>
+					<Button className={classes['form--button']}>100</Button>
+				</div>
 			</div>
+		</div>
 	);
 };
 
