470a36cb7a4125b0aee32c225e894960d0856167
Cydo <cydoentis@gmail.com>
2022-10-25 00:35:53 -0400

    switched resolve to join

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
